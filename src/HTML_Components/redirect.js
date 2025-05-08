8484ebb62438dda8f567b89862dc55e4c85cbeff
watsana <watsana.mue@dome.tu.ac.th>
2025-05-08 18:24:30 +0700

    Nothing

--- src/HTML_Components/redirect.js
+++ src/HTML_Components/redirect.js
@@ -1,4 +1,5 @@
 /* Redirection : ใช้เปลี่ยนไปหน้าต่างๆ ตาม 'des' ที่รับเข้ามา ไว้ใช้กับพวก side bar, top bar*/
 function redirect(des) {
-    window.location.href = des;
+    window.location.href = des;  // ปรับเปลี่ยนให้เป็น URL ที่คุณต้องการ
+    
 }
