9d4113cf86b085f121f32963dc8432d8d0a9b60b
thanapornTH15 <thanaporn.thongs@dome.tu.ac.th>
2025-05-11 21:10:06 +0700

    create + setting: checkin-out & payments pages

--- src/HTML_Components/side-bar.js
+++ src/HTML_Components/side-bar.js
@@ -11,8 +11,8 @@ class SideBar extends HTMLElement {
                 </div>
                 <button class="menu" onclick="redirect('#')">Create Booking</button>                
                 <button class="menu" onclick="redirect('#')">Booking History</button>                
-                <button class="menu" onclick="redirect('#')">Check-In / Check-Out</button>                
-                <button class="menu" onclick="redirect('#')">Payments & Receipts</button>
+                <button class="menu" onclick="redirect('../Checkin_out/checkin.html')">Check-In / Check-Out</button>                
+                <button class="menu" onclick="redirect('../Payment/payment.html')">Payments & Receipts</button>
                 <button class="menu" onclick="redirect('#')">Customer & Pet Profiles</button>                
                 <button class="menu" onclick="redirect('#')">Room Report</button>                
                 <button class="menu" onclick="redirect('#')">Notification</button>                
